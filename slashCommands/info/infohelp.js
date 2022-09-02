7964d3e02766477eec7cbe13788db75f949a3c02
kyleedeveloper <92257881+kyleedeveloper@users.noreply.github.com>
2022-09-02 22:39:31 +0300

    Update infohelp.js

--- slashCommands/info/infohelp.js
+++ slashCommands/info/infohelp.js
@@ -9,7 +9,7 @@ module.exports = {
       let embed = new MessageEmbed()
       .setTitle(`${client.user.username} Ticket Yardım Sayfası`)
       .setDescription(`Botun Ticket Komutlarını Gösterir.\n\n**Ticket Komutları**\n\`ticket yardım\`\nTicket Yardım Sayfasını Gösterir.\n\n\`ping\`\nBotun Pingini Ölçer.\n\n\n**Slaş Komutları**\n\`ping\`\nBotun Pingini Ölçer.\n\n\`kategori\`\nTicketin Açılacağı Kanalı Ayarlar\n\n\`kanala-gonder\`\nTicket Panelini Atılacağı Kanalı Ayarlar\n\n\`transcript-ayarla\`\nTranscripti ve Transcript Türünün Atılacağı Kanalı Ayarlar\n\n\`panel-gonder\`\nTicketin Panelini Atar\n\n\`Ticket\`\nTicket Oluşturur\n\n\`Kapat\`\nTicketi Kapatır\n\n\`Ticketi-aç\`\nTicketi Açar.\n\n\`kapat\`\nTicketi Kapatır.\n\n\`transcript\`\nTicketin Transcriptini Atar `)
-      .setFooter(`Kylee Ticket Sistemi`)
+      .setFooter(`YusufiBOT Ticket Sistemi`)
       .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 4096 }))
       interaction.reply({ embeds: [embed] });
 	}
